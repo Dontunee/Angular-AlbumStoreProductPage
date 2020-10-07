8c9fc2cf835fa78ac069b0bf9230bef8db5470db
Babatunde Afolabi <tundeafolabi@users.noreply.github.com>
2020-10-07 01:29:08 +0100

    IncludeObservableInProductService

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http';
+import {Album} from './album';
 import 'rxjs/add/operator/map';
-
+import 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
